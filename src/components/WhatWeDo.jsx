14a1eff28338cb0eeeefc192a4b405a5af80639f
Justice Adam <justiceadam673@gmail.com>
2025-04-10 21:51:01 +0100

    another final oo

--- src/components/WhatWeDo.jsx
+++ src/components/WhatWeDo.jsx
@@ -7,7 +7,7 @@ const WhatWeDo = () => {
   return (
     <main className='w-full text-black/75 flex max-lg:flex-col  justify-center gap-[35px] items-center mt-[75px] px-6'>
       <section className='rounded-[23px] bg-white h-auto'>
-        <div className='max-w-[272px]  max-lg:w-full h-[388px] w-fit  '>
+        <div className='max-w-[272px] h-[388px] w-fit  '>
           <img src={HandShake} className='rounded-t-[23px]  h-fit ' />
           <section className='py-[15px] space-y-[10px] rounded-b-[23px] px-[25px] '>
             <h2 className='font-bold'>Partner with Vendors</h2>
@@ -31,7 +31,7 @@ const WhatWeDo = () => {
         </div>
       </section>
       <section className='rounded-[23px] bg-white h-auto'>
-        <div className='max-w-[272px]  w-fit  '>
+        <div className='max-w-[272px] h-[388px] w-fit  '>
           <img src={CollectGoods} className='rounded-t-[23px] h-fit ' />
           <section className='py-[15px] space-y-[10px] rounded-b-[23px] px-[25px] '>
             <h2 className='font-bold'>Power Logistics</h2>
